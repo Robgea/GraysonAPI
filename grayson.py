fc7bae9a81e7c1c93cd79c6c4843b33175cdb09c
Rob <Zhouenkissinger@gmail.com>
2019-05-04 18:07:35 -0400

    Added features. Debugging the get locations method.

--- grayson.py
+++ grayson.py
@@ -10,12 +10,12 @@ class GraysonAPI:
         self.API_key = API_key
         self.org_id = org_id
         self.header = {'Authorization' : f'Access-Token {API_key}'}
-        self.url = 'https://api.robinpowered.com/v1.0/organizations/'
+        self.url = 'https://api.robinpowered.com/v1.0/'
         
 
-
+    # Organizations work
     def get_users(self):
-        users_output = requests.get(f'{self.url}{self.org_id}/users', headers = self.header)
+        users_output = requests.get(f'{self.url}organizations/{self.org_id}/users', headers = self.header)
         return users_output.text
 
     def add_user(self, **kwargs):
@@ -23,15 +23,25 @@ class GraysonAPI:
         invite_email = kwargs['email']
         # add error catch here
         params = {'name': invite_name, 'email': invite_email, 'format': 'json'}
-        new_user = requests.post(f'{self.url}{self.org_id}/users', headers = self.header, data = params)
+        new_user = requests.post(f'{self.url}organizations/{self.org_id}/users', headers = self.header, data = params)
         return new_user.text
 
+        # gotta do error handling
+        # If it 409s it still sends an invite. Is that deliberate?
 
-    # def get_locations(self):
-    #     locations_output = requests.get(f'{self.url}{self.org_id}/locations', headers = self.header)
-    #     return locations_output.text
 
-    # def get_amenities(self):
-    #     amenities_output = requests.get(f'{self.url}{self.org_id}/amenities', headers = self.header)
-    #     loaded = json.dump(amenities_output)
-    #     return loaded'''
+    def get_locations(self):
+        locations_output = requests.get(f'{self.url}organizations/{self.org_id}/locations', headers = self.header)
+        loaded_locations = json.loads(locations_output.text)
+        if loaded_locations['meta']['status_code'] == 200:
+            return loaded_locations['data']
+
+        else:
+            print('Hey hey, we have an error!  ')
+            print(f'Error code received. Code: {loaded_locations['meta']['status_code']} ')
+
+
+    def get_amenities(self):
+        amenities_output = requests.get(f'{self.url}organizations/{self.org_id}/amenities', headers = self.header)
+        loaded = json.loads(amenities_output.text)
+        return loaded
