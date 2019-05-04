781311d1d00cb69867a60409af9cefb9f163957b
Rob <Zhouenkissinger@gmail.com>
2019-05-04 19:03:07 -0400

    removed a debug line in the get locations method

--- grayson.py
+++ grayson.py
@@ -17,7 +17,14 @@ class GraysonAPI:
     def get_users(self):
         users_output = requests.get(f'{self.url}organizations/{self.org_id}/users', headers = self.header)
         loaded_users = json.loads(users_output.text)
-        return loaded_users['data']
+        if loaded_users['meta']['status_code'] == 200:
+            return loaded_users['data']
+
+        else:
+            print('Hey hey, we have an error!  ')
+            print(f'Error code received. \n Code: {loaded_users["meta"]["status_code"]} \n Message:  {loaded_users["meta"]["message"]} ')
+
+
 
     def add_user(self, **kwargs):
         invite_name = kwargs['name']
@@ -25,21 +32,26 @@ class GraysonAPI:
         # add error catch here
         params = {'name': invite_name, 'email': invite_email, 'format': 'json'}
         new_user = requests.post(f'{self.url}organizations/{self.org_id}/users', headers = self.header, data = params)
-        return new_user.text
+        loaded_user_return = json.loads(new_user.text)
+        if loaded_user_return['meta']['status_code'] == 200:
+            return f"Success, {invite_name} at {invite_email} has been invited into the organization."
+        else: 
+
+            return new_user.text
 
         # gotta do error handling
         # If it 409s it still sends an invite. Is that deliberate?
 
 
     def get_locations(self):
-        locations_output = requests.get(f'{self.url}organizaXXtions/{self.org_id}/locations', headers = self.header)
+        locations_output = requests.get(f'{self.url}organizations/{self.org_id}/locations', headers = self.header)
         loaded_locations = json.loads(locations_output.text)
         if loaded_locations['meta']['status_code'] == 200:
             return loaded_locations['data']
 
         else:
             print('Hey hey, we have an error!  ')
-            print(f'Error code received. Code: {loaded_locations["meta"]["status_code"]} ')
+            print(f'Error code received. \n Code: {loaded_locations["meta"]["status_code"]} \n Message:  {loaded_locations["meta"]["message"]} ')
 
 
     def get_amenities(self):
