2b62a01acb3d8697e363243b79dfccf5238be37e
Rob <Zhouenkissinger@gmail.com>
2019-05-04 22:30:03 -0400

    All organization methods added, almost finished.

--- grayson.py
+++ grayson.py
@@ -14,11 +14,27 @@ class GraysonAPI:
         
 
     # Organizations work
+
+    
+
+    # raw user info
     def get_users(self):
         users_output = requests.get(f'{self.url}organizations/{self.org_id}/users', headers = self.header)
         loaded_users = json.loads(users_output.text)
         if loaded_users['meta']['status_code'] == 200:
             return loaded_users['data']
+
+        else:
+            print('Hey hey, we have an error!  ')
+            print(f'Error code received. \n Code: {loaded_users["meta"]["status_code"]} \n Message:  {loaded_users["meta"]["message"]} ')
+    
+    #parsed user info
+    def get_user_emails(self):
+        users_output = requests.get(f'{self.url}organizations/{self.org_id}/users', headers = self.header)
+        loaded_users = json.loads(users_output.text)
+        if loaded_users['meta']['status_code'] == 200:
+            ouput_dict = { user['name'] : user['primary_email']['email'] for user in loaded_users['data']}
+            return ouput_dict
 
         else:
             print('Hey hey, we have an error!  ')
@@ -37,7 +53,7 @@ class GraysonAPI:
             return f"Success, {invite_name} at {invite_email} has been invited into the organization."
         else: 
 
-            return new_user.text
+            return loaded_user_return
 
         # gotta do error handling
         # If it 409s it still sends an invite. Is that deliberate?
@@ -54,7 +70,50 @@ class GraysonAPI:
             print(f'Error code received. \n Code: {loaded_locations["meta"]["status_code"]} \n Message:  {loaded_locations["meta"]["message"]} ')
 
 
+    def add_location(self, **kwargs):
+        location_info = {'name': '', 'description': '', 'image' : '', 'format': 'json'}
+        if 'name' not in kwargs:
+            print('No name given, invalid input!')
+            return "ERROR ERROR ERROR"
+
+        location_info['name'] = kwargs['name']
+
+        if 'description' in kwargs:
+            location_info['description'] = kwargs['description']
+
+        if 'address' in kwargs:
+            if len(kwargs['address']) < 5:
+                print("Address is too short, please write full address.")
+                return "Error Error"
+            else:
+                location_info.update({'address' : kwargs['address']})
+
+
+        if 'image' in kwargs:
+            if not kwargs['image'].startswith('http'):
+                print('Image file is not a web address. Try again')
+                return 'ERROR!'
+            else:
+                location_info['image'] = kwargs['image']
+
+        if 'time_zone' in kwargs:
+            location_info['time_zone'] = kwargs['time_zone']
+
+        new_location = requests.post(f'{self.url}organizations/{self.org_id}/locations', 
+            headers = self.header, data = location_info)
+
+        loaded_new_location_return = json.loads(new_location.text)
+        if loaded_new_location_return['meta']['status_code'] == 200:
+            return f'Success! New room {location_info["name"]} has been added!'
+        else:
+            return loaded_new_location_return
+
     def get_amenities(self):
         amenities_output = requests.get(f'{self.url}organizations/{self.org_id}/amenities', headers = self.header)
-        loaded = json.loads(amenities_output.text)
-        return loaded
+        loaded_amenities = json.loads(amenities_output.text)
+        if loaded_amenities['meta']['status_code'] == 200:
+            return loaded_amenities['data']
+
+        else:
+            print('Hey hey, we have an error!  ')
+            print(f'Error code received. \n Code: {loaded_locations["meta"]["status_code"]} \n Message:  {loaded_locations["meta"]["message"]} ')
--- readme.md
+++ readme.md
@@ -8,11 +8,15 @@ This is being done as a personal project and without the support of the Robin st
 
 * Logs you into the Robin API (you need a valid API key and your organization name to do this.)
 
-* Get a user list for your organization
+* Get a user list for your organization.
 
-* Add a user to your organization
+* Get a list of user emails in your organization.
+
+* Add a user to your organization.
 
 * Get a list of locations in your organization.
+
+* Add a location to your organization.
 
 * Get a list of amenities in your organization.
 
@@ -23,3 +27,15 @@ All of this is very rough right now.
 # Example of use:
 
 I'll update this with examples of this in use in the very near future.
+
+
+
+## Bugs?
+
+API doesn't check to see if Locations already exist with same data. Would be nice to remove?
+
+
+## Questions
+
+Anyway to change the hours in a location from the API?
+Anyway to set the timezone for a location from the API?
