c682f03af56a2d2767dca6e64750157116193fc2
Rob <Zhouenkissinger@gmail.com>
2019-05-06 11:10:38 -0400

    All Organizations methods now housed here, and tested.

--- organization.py
+++ organization.py
@@ -22,3 +22,69 @@ class Organization(object):
     def get_amenities(self):
         amenities_output = self.client.get_method(branch = 'organizations', endpoint = 'amenities')
         return amenities_output
+
+    def add_user(self, **kwargs):
+        repeat = False
+        invite_name = kwargs['name']
+        invite_email = kwargs['email']
+
+        # An error catcher should be added here soon.
+        params = {'name': invite_name, 'email': invite_email, 'format': 'json'}
+
+        if 'repeat' in kwargs:
+            if kwargs['repeat'] == True:
+                repeat = True
+
+        #This section is used to make sure you're not reinviting someone accidentally. 
+        if repeat == False:
+            check_list = self.get_user_emails()
+            print(check_list)
+            if check_list == 'Error!':
+                print('Error occured when checking e-mail list. ')
+                # Placeholder error return.
+                return False
+            elif invite_email in check_list.values():
+
+                print('Email already in organization! \n If you want to send this invite set "repeat = true" during function call.')
+                return False
+            else:
+                pass
+
+        new_user = self.client.post_method(branch = 'organizations', endpoint = 'users', params = params)
+        
+        return new_user
+
+
+    def add_location(self, **kwargs):
+        location_info = {'name': '', 'description': '', 'image' : '', 'format': 'json'}
+        if 'name' not in kwargs:
+            print('No name given, invalid input!')
+            return "ERROR ERROR ERROR"
+
+        location_info['name'] = kwargs['name']
+
+        if 'description' in kwargs:
+            location_info['description'] = kwargs['description']
+
+        if 'address' in kwargs:
+            if len(kwargs['address']) < 5:
+                print("Address is too short, please write full address.")
+                return "Error Error"
+            else:
+                location_info.update({'address' : kwargs['address']})
+
+
+        if 'image' in kwargs:
+            if not kwargs['image'].startswith('http'):
+                print('Image file is not a web address. Try again')
+                return 'ERROR!'
+            else:
+                location_info['image'] = kwargs['image']
+
+        if 'time_zone' in kwargs:
+            location_info['time_zone'] = kwargs['time_zone']
+
+        #need to add support for other parameters. Open times, etc. Low priority.
+
+        new_location = self.client.post_method(branch = 'organizations', endpoint = 'locations', params = location_info)
+        return new_location
