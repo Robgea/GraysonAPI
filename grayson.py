fa198579c8a2d1e4c3338725515f4b225264bc80
Rob <Zhouenkissinger@gmail.com>
2019-05-06 11:10:04 -0400

    Removed all delegated methods.

--- grayson.py
+++ grayson.py
@@ -25,99 +25,26 @@ class GraysonAPI:
         return parsed_output
 
     def post_method(self, params = None, **kwargs):
+        if params == None:
+            params = {'format' : 'json'}
 
-        new_user = requests.post(f'{self.url}{branch}/{self.org_id}/{endpoint}', headers = self.header, data = params)
-        pass
+        branch = kwargs['branch']
+        endpoint = kwargs['endpoint']
+
+        output = requests.post(f'{self.url}{branch}/{self.org_id}/{endpoint}', headers = self.header, data = params)
+        parsed_output = self.return_parser(output)
+        return parsed_output
 
     def return_parser(self, response):
         read_output = json.loads(response.text)
-        if read_output['meta']['status_code'] == 200:
+        if str(read_output['meta']['status_code']).startswith('20'):
             return read_output['data']
 
         else:
             # A real error handler will come here eventually. Right now I'm doing this.
             print('Hey hey, we have an error!  ')
-            return f'Error code received. \n Code: {loaded_users["meta"]["status_code"]} \n Message:  {loaded_users["meta"]["message"]} '
+            return f'Error code received. \n Code: {read_output["meta"]["status_code"]} \n Message:  {read_output["meta"]["message"]} '
 
-
-    def add_user(self, **kwargs):
-        repeat = False
-        invite_name = kwargs['name']
-        invite_email = kwargs['email']
-
-        # add error catch here
-        params = {'name': invite_name, 'email': invite_email, 'format': 'json'}
-
-        if 'repeat' in kwargs:
-            if kwargs['repeat'] == True:
-                repeat = True
-
-        #This section is used to make sure you're not reinviting someone accidentally. 
-        if repeat == False:
-            check_list = self.get_user_emails()
-            print(check_list)
-            if check_list == 'Error!':
-                print('Error occured when checking e-mail list. ')
-                # Placeholder error return.
-                return False
-            elif invite_email in check_list.values():
-
-                print('Email already in organization! \n If you want to send this invite set "repeat = true" during function call.')
-                return False
-            else:
-                pass
-
-        print('Sending user creation...')
-        new_user = requests.post(f'{self.url}organizations/{self.org_id}/users', headers = self.header, data = params)
-        loaded_user_return = json.loads(new_user.text)
-        if loaded_user_return['meta']['status_code'] == 200:
-            return f"Success, {invite_name} at {invite_email} has been invited into the organization."
-        else: 
-            return loaded_user_return
-
-
-        # If it 409s it still sends an invite. Is that deliberate?
-
-
-    def add_location(self, **kwargs):
-        location_info = {'name': '', 'description': '', 'image' : '', 'format': 'json'}
-        if 'name' not in kwargs:
-            print('No name given, invalid input!')
-            return "ERROR ERROR ERROR"
-
-        location_info['name'] = kwargs['name']
-
-        if 'description' in kwargs:
-            location_info['description'] = kwargs['description']
-
-        if 'address' in kwargs:
-            if len(kwargs['address']) < 5:
-                print("Address is too short, please write full address.")
-                return "Error Error"
-            else:
-                location_info.update({'address' : kwargs['address']})
-
-
-        if 'image' in kwargs:
-            if not kwargs['image'].startswith('http'):
-                print('Image file is not a web address. Try again')
-                return 'ERROR!'
-            else:
-                location_info['image'] = kwargs['image']
-
-        if 'time_zone' in kwargs:
-            location_info['time_zone'] = kwargs['time_zone']
-
-        #need to add support for other parameters. Openings, etc.
-
-        new_location = requests.post(f'{self.url}organizations/{self.org_id}/locations', 
-            headers = self.header, data = location_info)
-
-        loaded_new_location_return = json.loads(new_location.text)
-        if loaded_new_location_return['meta']['status_code'] == 200:
-            return f'Success! New room {location_info["name"]} has been added!'
-        else:
-            return loaded_new_location_return
 
 
     # locations start here.
